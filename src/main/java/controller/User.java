5da78c17e23144b1589a4f0306d1dadda10834bf
xueke <xuekepei@gmail.com>
2023-03-20 16:01:16 +0900

    :art: 基础配置

---
+++ src/main/java/controller/User.java
@@ -0,0 +1,21 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("")
+public class User {
+
+    //��¼ģ�� //����Ҫ���ò���ͬʱ��¼��̨�豸���ǣ�Ҳ���Ǽ�¼һ���ֶΣ�
+    @RequestMapping("/login")
+    public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("12345");
+
+    }
+}
---
+++ src/main/java/controllers/auth/Auth.java
@@ -0,0 +1,25 @@
+package controllers.auth;
+
+import common.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import services.AuthService;
+
+@RestController
+@RequestMapping("")
+public class Auth {
+
+    @Autowired
+    protected AuthService authService;
+
+    //��¼ģ�� //����Ҫ���ò���ͬʱ��¼��̨�豸���ǣ�Ҳ���Ǽ�¼һ���ֶΣ�
+    @PostMapping("/login")
+    public User login(@Validated @RequestBody AuthRequest request) {
+        System.out.println("12345");
+        return this.authService.login(request.getAccount(), request.getNickname());
+    }
+}
